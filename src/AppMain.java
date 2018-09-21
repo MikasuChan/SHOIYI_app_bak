4672533a4714542e1fb12b719d68def4486c885e
MikasuChan <mikasu@protonmail.com>
2018-09-21 21:26:31 +0300

    Программа определения сообществ в социальных графах

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,5 +1,11 @@
-public class AppMain {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class AppMain {
+    public static void main(String[] args) throws IOException {
+        String access_token = "62a79c483277a08ab620cefaf1d5aed2ff6740780b0f2690058e26165766b64a59006accf250098015460";
+        VK vk = new VK(access_token);
+        String[] friends = vk.getFriends("113950718");
+        for (String friend : friends)
+           System.out.println(friend);
     }
 }
